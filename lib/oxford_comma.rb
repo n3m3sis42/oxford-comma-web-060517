dcd19d85d863ac4e635facc7a00215456a0810c9
Sarah Evans <sarah.g.evans42@gmail.com>
2017-05-15 23:02:01 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,8 @@
 def oxford_comma(array)
+  return array.join("") if array.size <=1
 
+  last_element = "and #{array.pop}"
+  array.push(last_element)
+
+  array.size <= 2 ? array.join(" ") : array.join(", ")
 end
